923ad3531d3f328cb5b8660f543b105dde2d337e
cb-muralidharank <94039717+cb-muralidharank@users.noreply.github.com>
2022-05-31 14:27:37 +0530

    Update Service.java

--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -8,86 +8,93 @@ import org.json.JSONObject;
 import org.springframework.http.HttpMethod;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+
+import static java.lang.System.out;
 
 public class Service {
-    private static final String BASE_URL = "https://www.workato.com/";
+    private static final String BASE_URL = "https://www.workato.com/api/managed_users/";
 
-    public static List createOrGetCustomer(String siteID, String email, Object manifestID, String apiKey) throws JSONException, IOException, InterruptedException {
-        JSONObject response1 = new JSONObject();
-        JSONObject response2 = new JSONObject();
-        JSONObject authObject = new JSONObject();
-        String fwCustomerName = siteID + "-wf";
-        authObject.put("type", "workato_auth");
-        if (true) {
-            String endpoint = "api/managed_users";
-            JSONObject integJSON = new JSONObject();
-            integJSON.put("name", siteID);
-            integJSON.put("notification_email", email);
-            integJSON.put("time_zone", "Central Time (US & Canada)");
-            integJSON.put("auth_settings", authObject);
-
-            JSONObject workflowJSON = new JSONObject();
-            workflowJSON.put("name", fwCustomerName);
-            workflowJSON.put("notification_email", email);
-            workflowJSON.put("time_zone", "Central Time (US & Canada)");
-            workflowJSON.put("auth_settings", authObject);
-            workflowJSON.put("whitelisted_apps", Collections.singletonList("Chargebee"));
-            HttpConnection connection1 = new HttpConnection(BASE_URL + endpoint, HttpMethod.POST.name(), integJSON.toString());
-            response1 = connection1.getResponse();
-            JSONObject res1 = new JSONObject((String) response1.get(HttpConnection.HTTP_RESPONSE));
-            if ((boolean) response1.get(HttpConnection.SUCCESS)) {
-                DataCache.SITE_TO_ID_MAP.put(res1.get("name"), res1.get("id"));
+    public String createOrGetCustomer(String siteID, String email, String manifestID, String apiKey, String folder) throws Exception {
+        try {
+            JSONObject authObject = new JSONObject();
+            authObject.put("type", "workato_auth");
+            if (!DataCache.SITE_TO_ID_MAP.containsKey(siteID)) {
+                out.println("Going to create Customer in workato");
+                JSONObject integJSON = new JSONObject();
+                integJSON.put("name", siteID);
+                integJSON.put("notification_email", email);
+                integJSON.put("time_zone", "Kolkata");
+                integJSON.put("auth_settings", authObject);
+//          workflowJSON.put("whitelisted_apps", Collections.singletonList("Chargebee"));
+                HttpConnection connection = new HttpConnection(BASE_URL, HttpMethod.POST.name(), integJSON.toString());
+                JSONObject response = connection.getResponse();
+                pushAndStartRecipes(response, manifestID, siteID, apiKey, email, true, folder);
+            } else {
+                HttpConnection connection = new HttpConnection(BASE_URL + DataCache.SITE_TO_ID_MAP.get(siteID), HttpMethod.GET.name(), null);
+                JSONObject response = connection.getResponse();
+                pushAndStartRecipes(response, manifestID, siteID, apiKey, email, false, folder);
             }
-//            HttpConnection connection2 = new HttpConnection(BASE_URL + endpoint, HttpMethod.POST.name(), workflowJSON.toString());
-//            response2 = connection2.getResponse();
-//            JSONObject res2 = new JSONObject((String) response2.get(HttpConnection.HTTP_RESPONSE));
-//            if ((boolean) response2.get(HttpConnection.SUCCESS)) {
-//                DataCache.SITE_TO_ID_MAP.put(res2.get("name"), res2.get("id"));
-//            }
-//            System.out.println("CUSTOMER ID: "+res1.get("id")+"FOR CUSTOMER "+res1.get("name")+"   "+"CUSTOMER_WORKFLOW_ID : "+res2.get("id")+"FOR CUSTOMER "+res2.get("name"));
-            Object folderID = createAndGetFolderID("mailchimp", res1.get("id"));
-            System.out.println("CUSTOMER ID: " + res1.get("id") + "  FOLDER ID: " + folderID + " for folder mailchimp");
-            callImportRecipe(manifestID, res1.get("id"), folderID);
-
-            JSONObject r = createUpdateConnection(res1.get("id"), siteID, apiKey, email);
-        } else {
-            String endpoint = "api/managed_users/";
-            HttpConnection connection1 = new HttpConnection(BASE_URL + endpoint + DataCache.SITE_TO_ID_MAP.get(siteID), HttpMethod.GET.name(), null);
-            response1 = connection1.getResponse();
-            HttpConnection connection2 = new HttpConnection(BASE_URL + endpoint + DataCache.SITE_TO_ID_MAP.get(fwCustomerName), HttpMethod.GET.name(), null);
-            response2 = connection2.getResponse();
-
+            return "Successfully connected";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Connection failed";
         }
-
-        return Arrays.asList(response1, response2);
     }
 
-    private static Object createAndGetFolderID(String folderName, Object id) throws JSONException, IOException {
-        String endpoint = "/api/managed_users/" + id + "/folders";
+    public void pushAndStartRecipes(JSONObject response, String manifestID, String siteID, String apiKey, String email, boolean isNew, String project) throws Exception {
+        Callable c2 = () -> {
+            JSONObject responseObj = new JSONObject((String) response.get(HttpConnection.HTTP_RESPONSE));
+            Integer customerID = (Integer) responseObj.get("id");
+            out.println("Customer created..... Customer ID :: " + customerID);
+            if (isNew && (boolean) response.get(HttpConnection.SUCCESS)) {
+                DataCache.SITE_TO_ID_MAP.put(responseObj.get("name"), customerID);
+            }
+            out.println("Going to create folder for " + project);
+            Integer folderID1 = createAndGetFolderID(project, customerID);
+            out.println("Folder for " + project + " is created and created Folder ID  :: " + folderID1);
+            out.println("Going to import project manifest into customer project folder");
+            callImportRecipe(manifestID, customerID, folderID1);
+            out.println("Successfully invoked import manifest API");
+            out.println("Going to update Chargebee connection");
+            createUpdateConnection(customerID, siteID, apiKey, email, folderID1);
+            out.println("Chargebee connection Authenticated successfully....");
+            out.println("Going to start the recipe in customer project folder....");
+            boolean isThereErrors = changeRecipeState(customerID, folderID1, true);
+            if (isThereErrors) {
+                out.println("Error in starting all the recipe in project folder....");
+            } else {
+                out.println("All the recipe in customer project folder is started....");
+            }
+            return null;
+        };
+        c2.call();
+    }
+
+    public Integer createAndGetFolderID(String folderName, Integer id) throws JSONException, IOException {
         JSONObject folderJSON = new JSONObject();
         folderJSON.put("name", folderName);
-        JSONObject response1 = new HttpConnection(BASE_URL + endpoint, HttpMethod.POST.name(), folderJSON.toString()).getResponse();
-        JSONObject res = new JSONObject((String) response1.get(HttpConnection.HTTP_RESPONSE));
-        return res.get("id");
+        JSONObject response = new HttpConnection(BASE_URL + id + "/folders", HttpMethod.POST.name(), folderJSON.toString()).getResponse();
+        JSONObject res = new JSONObject((String) response.get(HttpConnection.HTTP_RESPONSE));
+        return (Integer) res.get("id");
     }
 
-    public static JSONObject callImportRecipe(Object manifestID, Object customerID, Object folderID) throws JSONException, IOException {
+    public void callImportRecipe(String manifestID, Integer customerID, Integer folderID) throws JSONException, IOException {
         JSONObject custFolderJSON = new JSONObject();
         custFolderJSON.put("customer_id", customerID);
         custFolderJSON.put("folder_id", folderID);
         JSONObject wrapperJSON = new JSONObject();
         wrapperJSON.put("manifest_id", manifestID);
         wrapperJSON.put("customers", custFolderJSON);
-        return new HttpConnection("https://www.workato.com/webhooks/rest/09424938-2a16-4966-9fa2-39e4c366bac8/import_customer_recipes", HttpMethod.POST.name(), wrapperJSON.toString()).getResponse();
+        new HttpConnection("https://www.workato.com/webhooks/rest/09424938-2a16-4966-9fa2-39e4c366bac8/import_customer_recipes", HttpMethod.POST.name(), wrapperJSON.toString()).getResponse();
     }
 
-    public static JSONObject createUpdateConnection(Object managedCustomer, String subDomain, String apiKey, String email) throws IOException, JSONException, InterruptedException {
-        String endpoint = "https://www.workato.com/api/managed_users/" + managedCustomer + "/connections";
-        JSONObject connectionObject = getObject(endpoint);
-        if (connectionObject == null) return null;
+    public void createUpdateConnection(Object managedCustomer, String subDomain, String apiKey, String email, Object folderID) throws IOException, JSONException, InterruptedException {
+        String endpoint = BASE_URL + managedCustomer + "/connections";
+        JSONObject connectionObject = getConnectionObject(endpoint + "?folder_id=" + folderID);
+        if (connectionObject == null) return;
         String connectionID = connectionObject.optString("id");
         String name = connectionObject.optString("name");
         String provider = connectionObject.optString("provider");
@@ -101,10 +108,10 @@ public class Service {
         input.put("api_key", apiKey);
         requestJSON.put("input", input);
         JSONObject responce = new HttpConnection(endpoint + "/" + connectionID, HttpMethod.PUT.name(), requestJSON.toString()).getResponse();
-        return new JSONObject((String) responce.get(HttpConnection.HTTP_RESPONSE));
+        new JSONObject((String) responce.get(HttpConnection.HTTP_RESPONSE));
     }
 
-    private static JSONObject getObject(String endpoint) throws IOException, InterruptedException, JSONException {
+    private JSONObject getConnectionObject(String endpoint) throws IOException, InterruptedException, JSONException {
         int k = 10;
         while (k-- > 0) {
             JSONObject response1 = new HttpConnection(endpoint, HttpMethod.GET.name(), null).getResponse();
@@ -115,5 +122,49 @@ public class Service {
             Thread.sleep(2000);
         }
         return null;
+    }
+
+    public boolean changeRecipeState(Integer managedUserID, Integer folderId, boolean start) throws IOException, JSONException {
+        List<String> recipeIds = getRecipeIdsFromFolder(managedUserID, folderId);
+        List<String> errorInActionRecipes = new ArrayList<>();
+        for (String recipeId : recipeIds) {
+            out.println("Going to " + (start ? "start" : "stop") + " recipe :: " + recipeId);
+            if (!startOrStopRecipe(managedUserID, recipeId, start)) {
+                errorInActionRecipes.add(recipeId);
+                out.println("failed action :::::  " + recipeId);
+            } else {
+                out.println("success action :::::  " + recipeId);
+            }
+        }
+        return !errorInActionRecipes.isEmpty();
+    }
+
+    public boolean startOrStopRecipe(Integer managedUserID, String recipeId, boolean start) throws IOException, JSONException {
+        String endpoint = BASE_URL + managedUserID + "/recipes/" + recipeId + (start ? "/start" : "/stop");
+        JSONObject response = new HttpConnection(endpoint, HttpMethod.PUT.name(), null).getResponse();
+        JSONObject responce = new JSONObject((String) response.get(HttpConnection.HTTP_RESPONSE));
+        return responce.optBoolean("success");
+    }
+
+    private List<String> getRecipeIdsFromFolder(Integer managedUserID, Integer folderId) throws IOException, JSONException {
+        out.println("Going to fetch all the recipes in the project folder :: " + folderId);
+        String endpoint = BASE_URL + managedUserID + "/recipes?folder_id=" + folderId;
+        JSONObject response = new HttpConnection(endpoint, HttpMethod.GET.name(), null).getResponse();
+        JSONObject responce = new JSONObject((String) response.get(HttpConnection.HTTP_RESPONSE));
+        JSONArray jsonArray = (JSONArray) responce.get("result");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); ++i) {
+            list.add((jsonArray).optJSONObject(i).optString("id"));
+        }
+        out.println("Number of recipes found in the folder :: " + list.size());
+        return list;
+    }
+
+    public String disconnect(Integer managedUserID, Integer folderID) throws JSONException, IOException {
+        boolean isThereErrors = changeRecipeState(managedUserID, folderID, false);
+        if (isThereErrors) {
+            return "Error in stopping all the recipe in project folder.... ";
+        }
+        return "All the recipe in customer project folder is stopped....";
     }
 }
